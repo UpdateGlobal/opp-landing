abde0596c2586911fabcd52c6f1ee97f3cdf8500
Frontend Update <arte@update.pe>
2018-06-26 18:07:27 -0500

    add exito

---
+++ exito.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title>Edificio GRAU 355 | Vive en el Corazón de Miraflores</title>
+<?php include('includes/head-index.php'); ?>
+</head>
+<body>
+    <div class="page-wrapper">
+    <!-- Preloader -->
+<!--     <div class="preloader"></div> -->
+    <!-- Main Header-->
+    <header class="main-header style-v1">
+    	
+        <!--Header Top-->
+        <?php include ('includes/topbar.php'); ?>
+        <?php include ('includes/sticky.php') ;?>
+    
+    </header>
+    <!--End Main Header -->
+<body>
+
+<div class="container-fluid">
+    <div class="row" align="center">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="sec-title centered">
+        <i class="far fa-thumbs-up" style="font-size: 200px; margin-bottom: 20px"></i>
+        <h2>Mensaje enviado con exito, En la manera <br>de posible nos contactaremos con usted</h2>
+        <a href="index.php" class="theme-btn btn-style-four" style="color: grey;">Volver atras</a>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>  
+    </div>  
+</div>
+
+
+
+
+<?php include ('includes/footer.php'); ?>
+
+
+<!--End pagewrapper-->
+<?php include ('includes/scripts.php'); ?>
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-59d8334b2f20c262"></script>
+</body>
+</html>
